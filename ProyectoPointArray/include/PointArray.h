0df413a063e5828ad9e0014f21bb736b3bd7f3cd
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-06-08 15:50:17 -0500

    PointArray

--- ProyectoPointArray/include/PointArray.h
+++ ProyectoPointArray/include/PointArray.h
@@ -10,15 +10,19 @@ class PointArray : public Point
         PointArray(const Point points1[],const int size_pa);
         PointArray(const PointArray &pv);
         ~PointArray();
+
         void push_back(const Point &p);
         void insert(const int position,const Point &p);
         void remove(const int position);
         const int getSize();
         void clear();
+        Point *get(const int position);
+        const Point *get(const int pos)const;
 
     private:
         Point *points;
         int size_pointArray;
+        void resize(int size);
 };
 
 #endif // POINTARRAY_H
--- ProyectoPointArray/src/PointArray.cpp
+++ ProyectoPointArray/src/PointArray.cpp
@@ -2,20 +2,66 @@
 
 PointArray::PointArray()
 {
-    this -> size_pointArray = 0;
-    points = new Point[size_pointArray];
+    size_pointArray = 0;
+    points = new Point[0];
 }
 PointArray::PointArray(const Point points1[],const int size_pa)
 {
-    points = points1;
     this -> size_pointArray = size_pa;
+    points = new Point[size_pa];
+    for(int i = 0; i < size_pa; ++i){
+        points[i] = points1[i];
+    }
 }
-PointArray::PointArray(const PointArray &pv)
-{
-
+PointArray::PointArray(const PointArray &pv){
+    size_pointArray = pv.size_pointArray;
+    points = new Point[size_pointArray];
+    for(int i = 0; i < size_pointArray; i++){
+        points[i] = pv.points[i];
+    }
 }
 
-PointArray::~PointArray()
-{
 
+PointArray::~PointArray(){
+    delete[] points;
 }
+
+void PointArray::resize(int newSize){
+    Point *pts = new Point[newSize];
+    int minSize = ( newSize > size_pointArray ? size_pointArray : newSize );
+    for ( int i = 0; i < minSize ; i ++){
+        pts [i] = points [i]; }
+    delete [] points ;
+    size_pointArray = newSize ;
+    points = pts ;
+}
+void PointArray::clear(){
+    resize(0);
+}
+void PointArray::push_back(const Point &p){
+    resize(size_pointArray + 1);
+    points[size_pointArray - 1] = p;
+}
+void PointArray :: insert ( const int position , const Point &p) {
+    resize ( size_pointArray + 1);
+    for ( int i = size_pointArray - 1; i > position ; i --) {
+    points [i] = points [i -1];
+    }
+    points [ position ] = p;
+}
+void PointArray :: remove ( const int position ) {
+    if( position >= 0 && position < size_pointArray ) {
+    for( int i = position; i < size_pointArray - 2; i ++) {
+        points [i] = points [i + 1];
+        }
+    resize ( size_pointArray - 1);
+    }
+}
+Point * PointArray :: get ( const int position ) {
+    return position >= 0 && position < size_pointArray ? points + position : NULL ;
+}
+
+const Point * PointArray :: get ( const int position ) const {
+    return position >= 0 && position < size_pointArray ? points + position : NULL ;
+}
+
