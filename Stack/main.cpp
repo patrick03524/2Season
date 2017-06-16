0ef68071c14f374355f51d4fb51fd4cd75febc16
patrick03524 <dibujanteanime024@gmail.com>
2017-06-16 01:51:49 -0500

    Cuack003

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -43,9 +43,16 @@ template < class T>
 Stack <T> operator -( const Stack <T> &s1 , const Stack <T> &s2)
     {
     Stack <T> result = s1;
-    for ( unsigned i = 0; i < s1. items . size (); ++i) {
+    int temp;
+    for (unsigned int i = 0; i<s1.items.size();i++) {
         if(s1. items [i] == s2. items [i]){
-            result . items . pop_heap (s1. items [i]);
+            while(i<s1.items.size()-1){
+                temp = result.items [i+1];
+                result.items[i] = result.items[i+1];
+                result.items[i+1] = temp;
+                i++;
+            }
+            result . items . pop_back();
         }
     }
     return result ;
@@ -53,11 +60,34 @@ Stack <T> operator -( const Stack <T> &s1 , const Stack <T> &s2)
 
 int main()
 {
-    Stack<int> stack1, stack2,stackres;
+    cout <<"Primera Operacion"<<endl;
+    Stack<int> stack1, stack2,stackres,stackres2;
     stack1.push(8);
     stack2.push(5);
+    cout <<"Stack 1 : ";
+    stack1.getDatos(stack1);
+    cout <<" "<<endl;
+    cout <<"Stack 2 : ";
+    stack2.getDatos(stack2);
+    cout <<" "<<endl;
     stackres = (stack1+stack2);
+    cout <<"Stack 3, resultado de la Suma de ambos : ";
     stackres.getDatos(stackres);
+    cout <<" "<<endl;
+    cout <<"Segunda Operacion "<<endl;
+    stack1.push(6);
+    stack2.push(6);
+    stack1.push(5);
+    stack2.push(4);
+    cout <<"Stack 1 : ";
+    stack1.getDatos(stack1);
+    cout <<" "<<endl;
+    cout <<"Stack 2 : ";
+    stack2.getDatos(stack2);
+    cout <<" "<<endl;
+    stackres2 = (stack1-stack2);
+    cout <<"Stack 4, resultado de la Resta de ambos : ";
+    stackres2.getDatos(stackres2);
 
 
 }
