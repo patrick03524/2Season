017d461fdf945b6348cf34ab30ff6846e8ebe971
patrick03524 <dibujanteanime024@gmail.com>
2017-06-16 00:31:38 -0500

    Decorator2

--- Composite/main.cpp
+++ Composite/main.cpp
@@ -34,7 +34,7 @@ class Composite: public Component
     }
     void traverse()
     {
-        for (int i = 0; i < children.size(); i++)
+        for (unsigned int i = 0; i < children.size(); i++)
         // Polimorfismo
           children[i]->traverse();
     }
@@ -42,18 +42,21 @@ class Composite: public Component
 
 int main()
 {
-  Composite containers[4];
+    Composite containers[4];
 
-  for (int i = 0; i < 4; i++)
-    for (int j = 0; j < 3; j++)
-      containers[i].add(new Leaf(i *3+j));
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 3; j++)
+            containers[i].add(new Leaf(i *3+j)); //Guarda el espacio de memoria de la Clase Leaf y le da de parametros numeros del 0 al 12
 
-  for (int i = 1; i < 4; i++)
-    containers[0].add(&(containers[i]));
 
-  for (int i = 0; i < 4; i++)
-  {
-    containers[i].traverse();
-    cout << endl;
-  }
+    for (int i = 1; i < 4; i++)
+        containers[0].add(&(containers[i]));
+
+    for (int i = 0; i < 4; i++)
+    {
+        containers[i].traverse();
+        cout << endl;
+    }
+
+
 }
