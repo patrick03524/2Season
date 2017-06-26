f959884f373720dea53e9d3ac51986df1e77d12e
patrick03524 <dibujanteanime024@gmail.com>
2017-06-26 00:19:13 -0500

    ProyectoMamon

---
+++ PythonTest/codigo.py
@@ -0,0 +1,10 @@
+print "Hello World"
+
+print "clone project"
+print "clone project"
+print "clone project"
+
+print "clone project"
+
+print "clone project"
+print "clone project"print "Test Done" print "Closing Test..."
